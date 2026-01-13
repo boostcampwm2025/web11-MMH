fb22dcefd92f463b7e8c027b6de757b83e92e538
rwaeng <rwaeng0421@gmail.com>
2026-01-13 17:04:35 +0900

    feat(#71): params -> query로 조회 방식 변경
    - swagger 태그 추가

--- apps/api/src/answer-submission/answer-submission.controller.ts
+++ apps/api/src/answer-submission/answer-submission.controller.ts
@@ -1,21 +1,48 @@
 import {
   Controller,
   Get,
-  Param,
   ParseIntPipe,
+  Query,
   UnauthorizedException,
 } from '@nestjs/common';
 import { AnswerSubmissionService } from './answer-submission.service';
+import {
+  ApiOkResponse,
+  ApiOperation,
+  ApiQuery,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
+import { AnswerSubmissionResponseDto } from './dtos/answer-submission-response.dto';
 
+@ApiTags('AnswerSubmission')
 @Controller('answer-submission')
 export class AnswerSubmissionController {
   constructor(
     private readonly answerSubmissionService: AnswerSubmissionService,
   ) {}
 
-  @Get('list/:questionId')
+  @Get()
+  @ApiOperation({
+    summary: '문제별 제출 기록 조회',
+    description: '특정 문제에 대한 사용자의 제출 기록을 최신순으로 조회합니다.',
+  })
+  @ApiQuery({
+    name: 'questionId',
+    type: 'number',
+    required: true,
+    description: '조회할 문제 ID',
+    example: 1,
+  })
+  @ApiOkResponse({
+    description: '조회 성공',
+    type: [AnswerSubmissionResponseDto],
+  })
+  @ApiUnauthorizedResponse({
+    description: '로그인 필요',
+  })
   async getDailySubmissionListByQuestionId(
-    @Param('questionId', ParseIntPipe) questionId: number,
+    @Query('questionId', ParseIntPipe) questionId: number,
   ) {
     // TODO: userId 실제 값 가지고 오도록 수정 필요
     const userId = 1;
