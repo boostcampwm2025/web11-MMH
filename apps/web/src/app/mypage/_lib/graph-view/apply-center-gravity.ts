1291fcb930c511b2eddcf9d513c45a3a0a5c947b
Choi JunHo <duekzim@gmail.com>
2026-01-14 22:49:58 +0900

    chore: 잘못 작성한 주석 제거

--- apps/web/src/app/mypage/_lib/graph-view/apply-center-gravity.ts
+++ apps/web/src/app/mypage/_lib/graph-view/apply-center-gravity.ts
@@ -5,7 +5,6 @@ function applyCenterGravity(nodes: NodeMap, centerX: number, centerY: number) {
   for (const node of nodes.values()) {
     if (node.fx !== null) continue;
 
-    // 마찰력 적용시키기
     node.vx += (centerX - node.x) * PHISICS_CONSTANT.CENTER_GRAVITY;
     node.vy += (centerY - node.y) * PHISICS_CONSTANT.CENTER_GRAVITY;
   }
